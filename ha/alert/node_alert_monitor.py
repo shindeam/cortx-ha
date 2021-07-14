64ecc7dcd5c0d2871c9bd1da12671a73ab9a2fb0
Amol Shinde <amol.shinde@seagate.com>
2021-07-14 14:14:11 +0530

    EOS-20858: Pacemaker Alert further log replication for >3nodes.
    Signed-off-by: Amol Shinde <amol.shinde@seagate.com>

--- ha/alert/node_alert_monitor.py
+++ ha/alert/node_alert_monitor.py
@@ -19,6 +19,7 @@
 from cortx.utils.log import Log
 from ha.alert.alert_monitor import AlertMonitor
 from ha.alert.iem import IemGenerator
+from ha.execute import SimpleCommand
 
 class NodeAlertMonitor(AlertMonitor):
 
@@ -32,4 +33,39 @@ class NodeAlertMonitor(AlertMonitor):
         Log.debug("Processing event for NodeAlertMonitor")
         # Environment variable are avilable in self.crm_env
         self.iem = IemGenerator()
-        self.iem.generate_iem(self.crm_env["CRM_alert_node"], self.alert_event_module, self.alert_event_type)
+        alert_dup = AlertDuplication()
+        self.node_ids, self.local_node = alert_dup.AlertDuplication().get_status()
+
+        if self.node_ids[-1] == self.local_node:
+            self.iem.generate_iem(self.crm_env["CRM_alert_node"], self.alert_event_module, self.alert_event_type)
+            Log.info(f"Sent IEM alert from local node: {self.local_node}")
+        else:
+            Log.info(f"Not the highest node to sent IEM node failure alert.")
+
+class AlertDuplication:
+    """
+    Alert duplication.
+    """
+    def __init__(self):
+        """
+        Init method.
+        """
+        self.process = SimpleCommand()
+    
+    def get_status(self):
+        self._nodeids, self._err, self._rc = self.process.run_cmd("pcs status corosync | awk '{print $1}' | tail -n+5")
+        self._local_node, self._err, self._rc = self.process.run_cmd("crm_node -i")
+        Log.info(f"List of online node ids in cluster: {self._nodeids.split()}")
+        Log.info(f"Local node id: {self._local_node}")
+        self.nodeids_list = self._nodeids.split() 
+        self.sort_nodeids(self.nodeids_list, self._local_node)
+
+    def sort_nodeids(self, nodeids_list: list, local_node):
+        """
+        Sort node ids in ascending order.
+        """
+        self.nodeids_list = nodeids_list
+        self.local_node = local_node
+        self.nodeids_list.sort()
+        Log.info(f"Sorted node ids: {self.nodeids_list}")
+        return self.nodeids_list.sort(), self.local_node
